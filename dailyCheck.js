4e23c369e893ea1d38b0a5b0596bb34360d92a11
dh5harris <dh5harris@hotmail.com>
2019-08-06 22:42:44 -0500

    added validateLoginForm function

---
+++ dailyCheck.js
@@ -0,0 +1,16 @@
+function setFocus() {
+  document.getElementById('username').focus();
+}
+
+function validateLoginForm() {
+  if (document.getElementById('username').value == '') {
+    alert("Username cannot be blank");
+    document.getElementById('username').focus();
+    return false;
+  }
+  if (document.getElementById('password').value == '') {
+    alert('Password cannot be blank');
+    document.getElementById('password').focus();
+    return false;
+  }
+}
